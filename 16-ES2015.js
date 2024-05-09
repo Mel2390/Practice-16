ace00fcf7425856eaad3665bd01a2ff6891d6d73
Melanny <ziony23@gmail.com>
2024-05-08 17:30:27 -0700

    new file

---
+++ 16-ES2015.js
@@ -0,0 +1,25 @@
+
+
+
+A- const PI = 3.14
+
+B- const Age = 33;
+
+
+
+
+
+
+
+// What is the difference between var and let?
+//Variables declared by let are only available inside the block where they're defined. Variables declared by var are available throughout the function in which they're declared.
+
+  // What is the difference between var and const?
+  //The scope of a var variable is functional or global scope. The scope of a let variable is block scope. The scope of a const variable is block scope. It can be updated and re-declared in the same scope
+
+
+  //What is the difference between let and const?
+  //We use let when we know that the value of the variable may change over time, we use const when we want to create a variable that should not be reassigned.
+
+  //What is hoisting?
+  //is the default behavior in JavaScript where declarations of variables and functions are moved to the top of their respective scopes during the compilation phase. This ensures that regardless of where variables and functions are declared within a scope, they are accessible throughout that scope.
